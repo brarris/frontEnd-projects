29b5285f6a94fc7aa3c36b7893fc424277cba220
brarris <farisabrar62@gmail.com>
2025-10-20 11:34:27 +0700

    increment on the display of DOM by Id

--- 3-More Interactive/script.js
+++ 3-More Interactive/script.js
@@ -1,6 +1,9 @@
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+
 count = 0;
 
 function increment() {
   count += 1;
-  console.log(count);
+  countEl.innerText = count;
 }
