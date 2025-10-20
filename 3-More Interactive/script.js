2ab55cf162cf3209437730b28d5e85ff09097bd5
brarris <farisabrar62@gmail.com>
2025-10-20 11:56:55 +0700

    create save button, when finish count increment, then it can save on save value

--- 3-More Interactive/script.js
+++ 3-More Interactive/script.js
@@ -7,3 +7,11 @@ function increment() {
   count += 1;
   countEl.innerText = count;
 }
+
+// save-btn
+let saveBtn = document.getElementById("save-btn");
+console.log(saveBtn);
+
+function save() {
+  console.log(count);
+}
